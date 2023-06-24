e96f9a2688d3baec63c9e13706d098fd59a54165
Varun Tummala <varunt@gmail.com>
2023-06-23 21:16:35 -0400

    Varun Tummala script.js

--- script.js
+++ script.js
@@ -2,4 +2,7 @@
 //numberAdder should take 2 parameters and it should return the sum of those two values
 //After you're done, try looking around at the CSS and HTML to 
 //see if you understand what's going on :)
+function numberAdder(num1,num2){
+    return num1+num2;
+}
 
